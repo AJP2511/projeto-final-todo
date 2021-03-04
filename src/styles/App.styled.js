5fe235a9d8dd87fc353807acbfe7e5986f7d3e02
AJP2511 <johnson2511@gmail.com>
2021-03-04 12:49:44 -0300

    bgSolved

--- src/styles/App.styled.js
+++ src/styles/App.styled.js
@@ -1,15 +1,6 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  background: rgb(255, 177, 59);
-  background: linear-gradient(
-    156deg,
-    rgba(255, 177, 59, 1) 0%,
-    rgba(201, 39, 50, 1) 100%
-  );
-
-  height: 100vh;
-
   @media screen and (max-width: 720px) {
   }
 `;
